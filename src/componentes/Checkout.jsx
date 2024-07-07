cc62ad5b3b06a81d8e8df4005dac7330da40304e
Nicolas0903 <144188823+Nicolas0903@users.noreply.github.com>
2024-07-07 10:01:32 -0500

    Actualización Checkout.jsx-Leonardo

--- src/componentes/Checkout.jsx
+++ src/componentes/Checkout.jsx
@@ -1,52 +1,125 @@
 import Objetos from "../assets/Lista.json";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useNavigate} from "react-router-dom";
 import { Link } from 'react-router-dom';
 import Menu from './Menu'
 import Pie from './Pie'
 
 function Checkout() {
-
+    const { register, handleSubmit } = useForm();
+    const [statusCode, setStatusCode] = useState(null);
+    const navegar = useNavigate() 
+  
+  
+    async function onSubmit(values) {
+      // Aquí puedes usar values para enviar la información
+      await fetch('http://localhost:3080/api/tienda/newPedido',  {
+          method: "POST",
+          body: JSON.stringify(values),
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          }
+        })
+      .then( (res) => {
+        setStatusCode(res.status) 
+        if ( res.status === 201 ) {
+          console.log("********************")
+          navegar('/');
+        } else {
+          console.log(statusCode)
+        }
+      })
+      .finally(() => {
+        console.log("ya terminamos")
+      })
+  
+    }
     return (
         <>
         <Menu/>
         <div id='NumeroItems'>
             <p><h2>Casi Listo! Tu orden no estara completa hasta que revises y presiones el boton "Completar orden" al final de la pagina</h2></p>
         </div>
+        
     
         <div className='Contenedor'>
             <p>Datos de compra:</p>
         </div>
         <div class id='Envio'>
             <p>Direccion de envio:</p>
-            <input id="idaddress" name="Linea" tabindex="1" placeholder="Linea 1" required />
-            <input id="idaddress" name="Linea" tabindex="1" placeholder="Linea 2" required />
-            <input id="idaddress" name="Linea" tabindex="1" placeholder="Distrito" required />
-            <input id="idaddress" name="Linea" tabindex="1" placeholder="Ciudad" required />
-            <input id="idaddress" name="Linea" tabindex="1" placeholder="Pais" required />
+
+            <form onSubmit={handleSubmit(onSubmit)} className="my-form">
+      <label htmlFor="linea_1">Linea 1</label>
+      <input type="text" {...register("linea_1")} />
+      <label htmlFor="linea_2">Linea 2</label>
+      <input type="text" {...register("linea_2")} />
+      <label htmlFor="distrito">Distrito </label>
+      <input type="text" {...register("distrito")} />
+      <label htmlFor="ciudad">Ciudad</label>
+      <input type="text" {...register("ciudad")} />
+      <label htmlFor="pais">Pais</label>
+      <input type="text" {...register("pais")} />
+      <button type="submit">Grabar</button>
+    </form>
         </div>
 
         <div class id='Pago'>
             <p>Pago:</p>
-            <label for="idQR">Pago con QR</label>
-            <input type="radio" value="Pago con QR" name="tipo" id="idQR"></input>
-            <label for="idCC">Pago Tarjeta</label>
-            <input type="radio" value="Profesor" name="tipo" id="idCC"></input>
-            <input id="idaddress" name="Linea" tabindex="1" placeholder="Linea 1" required />
-            <input id="idaddress" name="Linea" tabindex="1" placeholder="Linea 2" required />
-            <input id="idaddress" name="Linea" tabindex="1" placeholder="Distrito" required />
-            <input id="idaddress" name="Linea" tabindex="1" placeholder="Ciudad" required />
+            <label htmlFor="idQR">Pago con QR</label>
+          <input
+            type="radio"
+            value="Pago con QR"
+            {...register("tipo_de_pago")}
+            id="tipo_de_pago"
+          />
+        
+        <label htmlFor="idQR">Pago con Tarjeta</label>
+          <input
+            type="radio"
+            value="Pago con Tarjeta"
+            {...register("tipo_de_pago")}
+            id="tipo_de_pago"
+          />
+        
+        
+        <label htmlFor="numero_de_tarjeta">Numero de tarjeta 1</label>
+      <input type="text" {...register("numero_de_tarjeta")} />
+     
+      <label htmlFor="vencimiento">Vencimiento 2</label>
+      <input type="text" {...register("vencimiento")} />
+      <label htmlFor="ccv">CCV </label>
+      <input type="text" {...register("ccv")} />
+      <label htmlFor="nombre_en_tarjeta">Nombre en tarjeta</label>
+      <input type="text" {...register("nombre_en_tarjeta")} />
+    
+
         </div> 
         <div className='Contenedor2'>
             <p>Metodo de envio:</p>
         </div>
         <div class id='TipoEnvio'>
-        <label for="idAereo">Economico Aereo - S/.10</label>
-            <input type="radio" value="Aereo" name="tipo" id="idAereo"></input>
-            <label for="idPriori">Envio Prioritario (5 a 10 dias)- S/.17.00</label>
-            <input type="radio" value="Prioritario" name="tipo" id="idPriori"></input>
+        <label htmlFor="metodo_de_envio">Envio Aereo</label>
+          <input
+            type="radio"
+            value="Envio Aereo"
+            {...register("metodo_de_envio")}
+            id="metodo_de_envio"
+            
+          />
+            <label htmlFor="metodo_de_envio">Envio Prioritario</label>
+          <input
+            type="radio"
+            value="Envio Prioritario"
+            {...register("metodo_de_envio")}
+            id="metodo_de_envio"
+          />
         </div>
         <div className='Contenedor'>
             <p>Datos de compra:</p>
+
         </div>
             <div id='s'>
             <p>Items en pedido:</p>
